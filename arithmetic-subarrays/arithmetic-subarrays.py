102059310d4333f6c75bcd52402f1244bd60859b
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-01-21 08:44:47 -0800

    Create arithmetic-subarrays.py

---
+++ arithmetic-subarrays/arithmetic-subarrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        output=[]
+        for i in range(len(l)):
+            check=[]
+            for j in range(l[i],(r[i])+1):
+                check.append(nums[j]) 
+            check.sort()
+            for k in range(len(check)-1):
+                delta=check[1]-check[0]
+                if(delta!=(check[k+1]-check[k])): 
+                    boolean=False
+                    break
+                else: boolean=True
+            output.append(boolean)
+        return output
