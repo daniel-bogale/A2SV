c5fee17b3bebc2b9bfb67aa12b5576f9447c4c91
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-01-31 14:51:01 -0800

    Update rotate-array.py

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def rotate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-
         k=k%len(nums)
         i=0
         j=len(nums)-1
