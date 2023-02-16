2f3cd78d922d99b585464ca54613271129c8f797
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-02-16 13:39:57 -0800

    Update sorting-a-sentence.py

--- SortingAsentence/sorting-a-sentence.py
+++ SortingAsentence/sorting-a-sentence.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def sortSentence(self, s):
-        arr = s.split(" ")
-        sortedSentence = ['']*len(arr)
-
-        for i in arr:
-            sortedSentence[int(i[-1])-1] = i[0:-1]
+        strArr = s.split()
+        length =len(strArr)
+        sorted = [""]*length
+        for word in strArr:
+            sorted[int(word[-1])-1] = word[0:-1]
         
-        return ((" ".join(sortedSentence)))
+        return (" ").join(sorted)
+    
+   
