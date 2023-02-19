dcebe89fdd7b43a1541559dd687e81f9ad18823e
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-02-19 02:00:29 -0800

    Update max-consecutive-ones-iii.py

--- max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def longestOnes(self,nums,k):
-        left=0
-        for right in range(len(nums)):
-            if nums[right]==0:
+        left = 0 
+        
+        for i in range(len(nums)):
+            if nums[i]==0:
                 k-=1
+
             if k<0:
                 if nums[left]==0:
                     k+=1
                 left+=1
-        return right-left+1
+            
+        return len(nums) - left 
