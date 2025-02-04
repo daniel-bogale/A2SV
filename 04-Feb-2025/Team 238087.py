7d87757851981ce38106484be0947a1547502fbe
Daniel Bogale <dannybg090909@gmail.com>
2025-02-04 09:18:19 +0300

    Created 'Team' by A2SV Hub

---
+++ 04-Feb-2025/Team 238087.py
@@ -0,0 +1,16 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+
+no_of_solution = 0
+for _ in range(n):
+    # ptr=list(map(int,input().split(" ")))
+    ptr = input().split(" ")
+    for i in range(len(ptr)):
+        ptr[i] = int(ptr[i])
+    
+    summation = sum(ptr)
+    if(summation >= 2):
+        no_of_solution+=1
+
+print(no_of_solution)
