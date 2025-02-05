38dacf689927bc5471fbe1c17362b54b426cafaa
Daniel Bogale <dannybg090909@gmail.com>
2025-02-05 08:25:33 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 241955.py
@@ -0,0 +1,8 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+N=int(input())
+array = list(map(int,input().split()))
+result = [""]*N
+for i,val in enumerate(array):
+    result[val-1] = str(i+1)
+print(" ".join(result))
