8065ce083e96046804bca5e94edc8c310d004b42
Daniel Bogale <dannybg090909@gmail.com>
2025-02-18 12:01:41 +0300

    Created 'Integer to Roman' by A2SV Hub

---
+++ 18-Feb-2025/Integer to Roman 270696.py
@@ -0,0 +1,47 @@
+# Problem: Integer to Roman - https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        int_roman = {
+            1: "I",
+            5: "V",
+            10: "X",
+            50: "L",
+            100: "C",
+            500: "D",
+            1000: "M",
+        }
+
+        max_sub_power = 0
+        while 10**(max_sub_power+1) <= num:
+            max_sub_power+=1
+
+        answer = ""
+        
+        while max_sub_power >= 0:
+            current_digit_value = num // (10**max_sub_power)
+            print(current_digit_value, num)
+            if current_digit_value < 4:
+                for i in range(current_digit_value):
+                    answer+= int_roman[10**max_sub_power]
+            elif current_digit_value == 4:
+                answer += int_roman[10**(max_sub_power)]
+                answer += int_roman[10**(max_sub_power)*5]
+
+            elif current_digit_value == 5:
+                answer+=int_roman[10**(max_sub_power)*5]
+
+            elif current_digit_value < 9:
+                answer+= int_roman[10**(max_sub_power)*5]
+                print("answer",max_sub_power, int_roman[10**(max_sub_power)*5])
+                addition = current_digit_value-5
+                for i in range(addition):
+                    answer+= int_roman[10**(max_sub_power)]
+            elif current_digit_value==9:
+                answer+= int_roman[10**(max_sub_power)]
+                answer+=int_roman[10**(max_sub_power+1)]
+
+            num -= current_digit_value * (10**max_sub_power)
+            max_sub_power -= 1
+
+        return answer
