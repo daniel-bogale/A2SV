9196175396deda0d00d552ea07addc7996687d1b
Daniel Bogale <dannybg090909@gmail.com>
2025-02-23 08:09:14 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 23-Feb-2025/A. Beautiful Matrix 280681.py
@@ -0,0 +1,11 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+index_of_one = (0,0)
+for i in range(5):
+    current_row = list(map(int,input().split()))
+    for idx,value in enumerate(current_row):
+        if value == 1:
+            index_of_one = (i,idx)
+    matrix.append(current_row)
+print(abs(2-index_of_one[0])+ abs(2-index_of_one[1]))
