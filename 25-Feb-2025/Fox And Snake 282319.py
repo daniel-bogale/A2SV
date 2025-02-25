d5d46836b1514e9f8abe19df44704dc0299bc4e8
Daniel Bogale <dannybg090909@gmail.com>
2025-02-25 13:32:59 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282319.py
@@ -0,0 +1,16 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+row, col = map(int, input().split())
+answer = [["."]*col for i in range(row)]
+for i in range(row):
+    if i % 2 == 0:
+        answer[i] = ["#"]*col
+        continue
+    elif i%4 == 1:
+        answer[i][-1] = "#"
+        continue
+    else:
+        answer[i][0] = "#"
+        continue
+for i in range(row):
+    print(("").join(answer[i]))
