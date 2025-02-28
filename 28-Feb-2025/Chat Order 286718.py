e459da2f08fdca7c00819b38b8b9d848fd7fdd3f
Daniel Bogale <dannybg090909@gmail.com>
2025-02-28 12:18:27 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 28-Feb-2025/Chat Order 286718.py
@@ -0,0 +1,14 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+N = int(input())
+chat_history = {
+    
+}
+for i in range(N):
+    person = input()
+    chat_history[person] = (person,i)
+
+history = [val for val in chat_history.values()]
+history = sorted(history, key=lambda val: val[1],reverse=True)
+for val in history:
+    print(val[0])
