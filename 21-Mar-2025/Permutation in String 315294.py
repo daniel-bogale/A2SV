87857714f846fe07918975ba35145925ac819f6c
Daniel Bogale <dannybg090909@gmail.com>
2025-03-21 23:03:36 +0300

    Created 'Permutation in String' by A2SV Hub

---
+++ 21-Mar-2025/Permutation in String 315294.py
@@ -0,0 +1,24 @@
+# Problem: Permutation in String - https://leetcode.com/problems/permutation-in-string/
+
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = Counter(s1)
+        window_count = Counter(s2[:len(s1)])
+        
+        if s1_count == window_count:
+            return True
+        
+        left = 0
+        for right in range(len(s1), len(s2)):
+            window_count[s2[right]] += 1  
+            window_count[s2[left]] -= 1 
+            if window_count[s2[left]] == 0:
+                del window_count[s2[left]]  
+            left += 1 
+            
+            if window_count == s1_count:
+                return True
+        
+        return False
