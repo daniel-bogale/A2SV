def003501abf60fb227ec9d791ecc34aa69f9092
Daniel Bogale <dannybg090909@gmail.com>
2025-04-07 09:14:38 +0300

    Created 'String Compression' by A2SV Hub

---
+++ 07-Apr-2025/String Compression 327731.py
@@ -0,0 +1,20 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+
+class Solution(object):
+    def compress(self,chars):
+        final_index=0
+        i=0
+        while i< len(chars):
+            letter=chars[i]
+            count=0
+            while i< len(chars) and chars[i]==letter:
+                count+=1
+                i+=1
+            chars[final_index]=letter 
+            final_index+=1
+            if count>1:
+                for c in str(count):
+                    chars[final_index]=c
+                    final_index+=1
+        return final_index
