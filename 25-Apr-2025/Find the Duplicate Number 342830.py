cfc016177a9107e496743cde690940b1c21b6069
Daniel Bogale <dannybg090909@gmail.com>
2025-04-26 00:12:40 +0300

    Created 'Find the Duplicate Number' by A2SV Hub

---
+++ 25-Apr-2025/Find the Duplicate Number 342830.py
@@ -0,0 +1,16 @@
+# Problem: Find the Duplicate Number - https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow,fast = 0,0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
