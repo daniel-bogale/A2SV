c8df3b0bbeaf234d5edc30d4ce343df5ea2dc861
Daniel Bogale <dannybg090909@gmail.com>
2025-04-26 23:31:23 +0300

    Created 'Clear Digits' by A2SV Hub

---
+++ 26-Apr-2025/Clear Digits 343168.py
@@ -0,0 +1,12 @@
+# Problem: Clear Digits - https://leetcode.com/problems/clear-digits/description/
+
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c.isdigit():
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
