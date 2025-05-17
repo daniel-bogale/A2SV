184a64c2be82f6c5a88682e00df90713e0d3e63b
Daniel Bogale <dannybg090909@gmail.com>
2025-05-17 21:50:57 +0300

    Created 'Minimum Number of Days to Make m Bouquets' by A2SV Hub

---
+++ 17-May-2025/Minimum Number of Days to Make m Bouquets 357578.py
@@ -0,0 +1,27 @@
+# Problem: Minimum Number of Days to Make m Bouquets - https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        def check(day):
+            bouquets, adj = 0, 0
+            for i in bloomDay:
+                if i<=day:
+                    adj+=1
+                else:
+                    adj=0
+                
+                if adj==k:
+                    bouquets+=1
+                    adj=0
+            return bouquets                    
+                    
+        i, j = 0, max(bloomDay)
+        res=-1
+        while i<=j:
+            mid=(i+j)//2
+            if check(mid)>=m:
+                res=mid
+                j=mid-1
+            else:
+                i=mid+1
+        return res
