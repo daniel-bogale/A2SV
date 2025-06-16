672c93bc0f57f4f66777d075e0ac45eee75bc1b0
Daniel Bogale <dannybg090909@gmail.com>
2025-06-16 22:52:00 +0300

    Created 'Maximum Erasure Value ' by A2SV Hub

---
+++ 16-Jun-2025/Maximum Erasure Value  367341.py
@@ -0,0 +1,18 @@
+# Problem: Maximum Erasure Value  - https://leetcode.com/problems/maximum-erasure-value/
+
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        seen = set()
+        left = 0
+        curr_sum = 0
+        max_sum = 0
+        for right in range(len(nums)):
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                curr_sum -= nums[left]
+                left += 1
+            seen.add(nums[right])
+            curr_sum += nums[right]
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
