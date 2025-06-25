9fa5c9f77f9d9e94655e3ac9168f1fc55e47e954
Daniel Bogale <dannybg090909@gmail.com>
2025-06-25 13:32:38 +0300

    Created 'Delete Node in a BST' by A2SV Hub

---
+++ 25-Jun-2025/Delete Node in a BST 370336.py
@@ -0,0 +1,39 @@
+# Problem: Delete Node in a BST - https://leetcode.com/problems/delete-node-in-a-bst/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: Optional[TreeNode]
+        :type key: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            temp = self.findMin(root.right)
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
+        
+        return root
+
+    def findMin(self, node):
+        while node.left:
+            node = node.left
+        return node
