18e433192e84a0801d6a531a5330c91e9dd2723e
Daniel Bogale <dannybg090909@gmail.com>
2025-08-11 15:01:06 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 11-Aug-2025/Permutations 383506.py
@@ -0,0 +1,21 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        result = []
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(n)
+            result.extend(perms)
+            nums.append(n)
+        return result
+
+
+
+
