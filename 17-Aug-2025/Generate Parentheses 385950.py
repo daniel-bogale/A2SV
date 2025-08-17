48a6e3a3cecd4384bae08451d84fe43d02b6b801
Daniel Bogale <dannybg090909@gmail.com>
2025-08-17 09:30:05 +0300

    Created 'Generate Parentheses' by A2SV Hub

---
+++ 17-Aug-2025/Generate Parentheses 385950.py
@@ -0,0 +1,16 @@
+# Problem: Generate Parentheses - https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(s="", left=0, right=0):
+            if len(s) == 2 * n:
+                res.append(s)
+                return
+            if left < n:
+                backtrack(s+"(", left+1, right)
+            if right < left:
+                backtrack(s+")", left, right+1)
+        
+        backtrack()
+        return res
