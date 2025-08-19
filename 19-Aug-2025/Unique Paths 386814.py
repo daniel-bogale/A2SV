8a52ede806bdf75f97fbd5317eb18a2741752442
Daniel Bogale <dannybg090909@gmail.com>
2025-08-19 16:58:45 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 19-Aug-2025/Unique Paths 386814.py
@@ -0,0 +1,5 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+          return math.comb(m + n - 2, m - 1)
