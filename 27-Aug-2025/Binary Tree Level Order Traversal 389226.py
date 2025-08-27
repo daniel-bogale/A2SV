0241b18049ef81058cdc9d081ed64432019d50c4
Daniel Bogale <dannybg090909@gmail.com>
2025-08-27 08:21:56 +0300

    Created 'Binary Tree Level Order Traversal' by A2SV Hub

---
+++ 27-Aug-2025/Binary Tree Level Order Traversal 389226.py
@@ -0,0 +1,26 @@
+# Problem: Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):   
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(level)
+        
+        return result
