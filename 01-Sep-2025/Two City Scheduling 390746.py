f935b913e7c78b0ef5cb7479dff6b1825c8f2465
Daniel Bogale <dannybg090909@gmail.com>
2025-09-02 00:23:39 +0300

    Created 'Two City Scheduling' by A2SV Hub

---
+++ 01-Sep-2025/Two City Scheduling 390746.py
@@ -0,0 +1,16 @@
+# Problem: Two City Scheduling - https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        diffs = []
+        for c1, c2 in costs:
+            diffs.append([c2-c1, c1,c2])
+        
+        diffs.sort()
+        res = 0 
+        for i in range(len(diffs)):
+            if i < len(diffs) // 2:
+                res += diffs[i][2]
+            else:
+                res +=diffs[i][1]
+        return res
