dac1369cf756780a0576cfbaffc539dd40af6c5b
Daniel Bogale <dannybg090909@gmail.com>
2025-09-04 05:25:16 +0300

    Created 'Minimum Replacements to Sort the Array' by A2SV Hub

---
+++ 04-Sep-2025/Minimum Replacements to Sort the Array 391206.py
@@ -0,0 +1,15 @@
+# Problem: Minimum Replacements to Sort the Array - https://leetcode.com/problems/minimum-replacements-to-sort-the-array/
+
+import math
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_val = nums[-1]
+        result = 0
+
+        for i in range(n - 2, -1, -1):
+            parts = math.ceil(nums[i] / max_val)
+            result += parts - 1
+            max_val = nums[i] // parts  
+
+        return result
