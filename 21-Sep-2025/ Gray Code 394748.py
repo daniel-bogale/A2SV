7a48185220f3b5b7be3c65dbc48cc6fa3923598d
Daniel Bogale <dannybg090909@gmail.com>
2025-09-21 21:48:09 +0300

    Created ' Gray Code' by A2SV Hub

---
+++ 21-Sep-2025/ Gray Code 394748.py
@@ -0,0 +1,9 @@
+# Problem:  Gray Code - https://leetcode.com/problems/gray-code/description/
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = [0] 
+        for i in range(n):
+            for j in range(len(res)-1, -1, -1):
+                res.append(res[j] | 1 << i)
+        return res 
