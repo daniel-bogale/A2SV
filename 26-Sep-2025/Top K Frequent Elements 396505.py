107659a9af09c683a73f16048ab60354fa22f91b
Daniel Bogale <dannybg090909@gmail.com>
2025-09-26 12:29:53 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 26-Sep-2025/Top K Frequent Elements 396505.py
@@ -0,0 +1,12 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        count=Counter(nums)
+        output=[]
+        inverse = [(value, key) for key, value in count.items()]
+        inverse.sort(reverse=True)
+        for i in range(0,k):
+            a=inverse[i][1]
+            output.append(a)
+        return output
